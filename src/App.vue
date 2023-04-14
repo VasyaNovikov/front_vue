c5e4475027a0df853e11267d8a42eb93933b94ee
vasiliinovikov <novonovo19777@gmail.com>
2023-04-14 10:54:21 +0400

    component dicomposition

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Creating a post</h4>
-      <input
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        class="input" type="text"
-        placeholder="post title"
-      >
-      <input
-        v-bind:value="body"
-        @input="body = $event.target.value"
-        class="input" type="text"
-        placeholder="post description"
-      >
-      <button class="btn" @click="createPost">Create</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>post title: </strong>{{ post.title }}</div>
-      <div><strong>post description: </strong>{{ post.body }}</div>
-    </div>
+    <post-form
+      @create="createPost"
+    />
+    <post-list
+      v-bind:posts="posts"
+      @remove="removePost"
+    />
   </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
 export default {
+  components: {
+    PostList, PostForm
+  },
   data() {
     return {
       posts: [
@@ -32,59 +25,23 @@ export default {
              {id: 2, title: 'javascript2', body: 'post description2'},
              {id: 3, title: 'javascript3', body: 'post description3'}
              ],
-             title: '',
-             body: '',
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      }
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post) {
+      this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
     }
-  },
-  inputTitle(event) {
-    this.title = event.target.value;
   }
 }
 </script>
 
-<style scoped>
+<style>
 * {
    margin: 0;
    padding: 0;
    box-sizing: border-box;
-}
-.post {
-   padding: 15px;
-   border: 2px solid teal;
-   margin-top: 15px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.app {
-  padding: 20px;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>A list of users</h3>
+    <post-item
+      v-for="post in posts"
+      v-bind:post="post"
+      v-bind:key="post.id"
+      @remove="$emit('remove', post)"
+    />
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  padding: 15px;
+}
+</style>
