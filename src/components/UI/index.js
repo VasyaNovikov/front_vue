6e80344375a4c66a89259c3433ec3363f09fb05a
vasiliinovikov <novonovo19777@gmail.com>
2023-04-22 13:00:05 +0400

    page navigation (router)

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,6 +3,7 @@ import MyInput from "@/components/UI/MyInput";
 import MyDialog from "@/components/UI/MyDialog";
 import MySelect from "@/components/UI/MySelect";
 
+
 export default [
     MyButton,
     MyInput,
