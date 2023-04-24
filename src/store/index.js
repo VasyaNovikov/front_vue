a7d486659b99648294800c1194918382e796820b
vasiliinovikov <novonovo19777@gmail.com>
2023-04-24 20:21:02 +0400

    modules in the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,8 @@
 import {createStore} from "vuex";
+import {postModule} from "@/store/postModule";
 
-export default createStore( {
-    state: {
-        likes: 2,
-        isAuth: true,
-    },
-    getters: {
-        doubleLikes(state) {
-            return state.likes * 2
-        }
-    },
-    mutations: {
-        incrementLikes(state) {
-            state.likes += 1
-        },
-        decrementLikes(state) {
-            state.likes -= 1
-        }
-    },
-    actions: {
-
-    },
+export default createStore({
     modules: {
-
+        post: postModule
     }
 })
